2c683a7ba7bdf27d827cae66e7015e4f6c97f3d9
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2015-10-20 16:56:19 -0600

    small changes

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -1,18 +1,6 @@
-# require_relative 'night_writer'
-# require_relative 'translation'
+require_relative 'translation'
 
-# handle = File.open("braille.txt", "r")
-#
-# handle.readline
-
-#
-# class NightReader
-#
-#   def initialize
-#   end
-
-
-  require_relative 'translation'
+class NightReader
 
   reader = File.open(read_file = "alpha_in.txt", "r")
   contents = reader.read
@@ -25,8 +13,6 @@
 
   writer.write line1 + line2 + line3
 
-  # writer.write "#{contents}\n#{contents}\n#{contents}"
-
   puts "I read #{reader.read.length} characters in #{read_file}."
 
-# end
+end
